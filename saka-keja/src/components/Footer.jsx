7f08af6209060808161e27a4ae0384615a62db1d
Rawllings Kiprop <rutojr01@gmail.com>
2023-01-27 12:06:23 +0300

    done with the footer

--- saka-keja/src/components/Footer.jsx
+++ saka-keja/src/components/Footer.jsx
@@ -2,26 +2,28 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div class="carousel-inner">
-        <h1 className="navbar-brand brand logoo ">
-          Saka-<span className="keja">keja</span>
-        </h1>
+    <footer>
+      <div className="footer">
+        <div class="carousel-inner">
+          <h1 className="navbar-brand brand logoo ">
+            Saka-<span className="keja">keja</span>
+          </h1>
+        </div>
+        <div className="footercontacts">
+          <h2 className="py-100">Contacts</h2>
+          <br></br>
+          <p></p>
+          <p></p>
+          <h5>+254727371100</h5>
+          <br />
+          <h5>sakakeja@gmail.com</h5>
+          <br />
+          <h5>Nairobi, Kenya</h5>
+        </div>
+        <div className="rights">
+          <p>Copyright ©2022 All rights reserved</p>
+        </div>
       </div>
-      <div className="footercontacts">
-        <h5>+254727371100</h5>
-        <br />
-        <h5>sakakeja@gmail.com</h5>
-        <br />
-        <h5>Nairobi, Kenya</h5>
-      </div>
-      <div className="rights">
-        <p>Copyright ©2022 All rights reserved</p>
-      </div>
-      <p className="landingWords py-0">
-        {" "}
-        Your next dream house is just one button away...
-      </p>
     </footer>
   );
 }
